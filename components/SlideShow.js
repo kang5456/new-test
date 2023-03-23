79bd162362ad13e2cafaf73a5e902005b6ea5c2b
kang5456 <kang5456@gmail.com>
2023-03-23 16:22:26 +0900

    footer fix

--- components/SlideShow.js
+++ components/SlideShow.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore, { Autoplay } from 'swiper'; // 이 줄을 수정합니다.
+import React, { useState, useEffect } from 'react';
+
 import styles from './SlideShow.module.css';
 
-SwiperCore.use([Autoplay]); // 이 줄을 추가합니다.
-
 const SlideShow = ({ slides }) => {
+  const [SwiperModule, setSwiperModule] = useState(null);
+  const [SwiperSlideModule, setSwiperSlideModule] = useState(null);
+
+  useEffect(() => {
+    import('swiper/react').then(({ Swiper, SwiperSlide }) => {
+      setSwiperModule(Swiper);
+      setSwiperSlideModule(SwiperSlide);
+    });
+  }, []);
+
+  if (!SwiperModule || !SwiperSlideModule) {
+    return null;
+  }
+
+  const Swiper = SwiperModule;
+  const SwiperSlide = SwiperSlideModule;
+
   return (
     <Swiper
       spaceBetween={50}
@@ -13,25 +27,29 @@ const SlideShow = ({ slides }) => {
       navigation
       pagination={{ clickable: true }}
       scrollbar={{ draggable: true }}
-      autoplay={{ delay: 3000 }} // 자동 넘어가기 기능을 활성화합니다.
+      autoplay={{
+        delay: 3000, // 자동으로 슬라이드 이동할 시간 간격 (밀리초)
+        disableOnInteraction: false // 슬라이드 이동 후 자동 슬라이드 기능을 일시 중지할 지 여부
+      }}
+      
     >
-    {slides.map(({ fields }, index) => {
-      const imageUrl = fields?.coverImage?.fields?.file?.url;
-      const title = fields.title;
-      const subtitle = fields.subTitle;
-      const authorName = fields.author.fields.name;
+      {slides.map(({ fields }, index) => {
+        const imageUrl = fields?.coverImage?.fields?.file?.url;
+        const title = fields.title;
+        const subtitle = fields.subTitle;
+        const authorName = fields.author.fields.name;
 
-      return (
-        <SwiperSlide key={index} className={styles.slide}>
-          {imageUrl && <img src={imageUrl} alt={title} />}
-          <div>
-            <h2>{title}</h2>
-            <h3>{subtitle}</h3>
-            <p>By {authorName}</p>
-          </div>
-        </SwiperSlide>
-      );
-    })}
+        return (
+          <SwiperSlide key={index} className={styles.slide}>
+            {imageUrl && <img src={imageUrl} alt={title} />}
+            <div>
+              <h2>{title}</h2>
+              <h3>{subtitle}</h3>
+              <p>By {authorName}</p>
+            </div>
+          </SwiperSlide>
+        );
+      })}
     </Swiper>
   );
 };
--- next.config.js
+++ next.config.js
@@ -6,26 +6,4 @@ module.exports = {
       process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
   },
   swcMinify: true,
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.module.rules.push({
-      test: /\.m?js/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            [
-              'next/babel',
-              {
-                'preset-env': {
-                  modules: 'commonjs',
-                },
-              },
-            ],
-          ],
-        },
-      },
-    });
-
-    return config;
-  },
 };
