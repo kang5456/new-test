49969bffebe1fbf8685366fc4de57d279d563d5c
kang5456 <kang5456@gmail.com>
2023-03-30 14:48:36 +0900

    slide show error

--- components/SlideShow.js
+++ components/SlideShow.js
@@ -9,9 +9,9 @@ const SlideShow = ({ slides }) => {
 
   const router = useRouter();
 
-  const handleClick = (type, title) => {
-    router.push(`/insights/${type}/${encodeURIComponent(title)}`);
-  };
+  const handleClick = (title) => {
+    router.push(`/insights/${title}`);
+  };  
 
   useEffect(() => {
     import('swiper/react').then(({ Swiper, SwiperSlide }) => {
@@ -37,22 +37,24 @@ const SlideShow = ({ slides }) => {
       loop // 무한 슬라이드 추가
       autoplay={{
         delay: 2000, // 자동으로 슬라이드 이동할 시간 간격 (밀리초)
+        disableOnInteraction: false, // 추가된 부분: 인터랙션 후에도 autoplay가 계속 작동합니다.
       }}
       
     >
       {slides.map(({ fields }, index) => {
-        const imageUrl = fields?.coverImage?.fields?.file?.url;
+        const imageUrl = fields?.cover?.fields?.file?.url;
+        const httpsImageUrl = imageUrl?.replace("http:", "https:"); // 추가된 부분: 이미지 URL의 http를 https로 변경
         const title = fields.title;
-        const subtitle = fields.type;
-        const authorName = fields.author; // 변경된 부분: 작성자 정보를 직접 액세스합니다.
+        const subtitle = fields.type; // 수정된 부분
+        const authorName = fields.author;
 
         return (
           <SwiperSlide 
             key={index} 
             className={styles.slide}
-            onClick={() => handleClick(fields.type, fields.title)}
+            onClick={() => handleClick(fields.title)}
           >
-            {imageUrl && <img src={imageUrl} alt={title} />}
+             {httpsImageUrl && <img src={httpsImageUrl} alt={title} />}
             <div>
               <h2>{title}</h2>
               <h3>{subtitle}</h3>
