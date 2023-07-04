7194a4d458d7d4c8bab43dc3454de30319befbaf
kang5456 <kang5456@gmail.com>
2023-07-04 14:03:12 +0900

    vercel 배포 오류 수정5

--- pages/posts/press.js
+++ pages/posts/press.js
@@ -44,7 +44,7 @@ function extractImageFromContent(content) {
 }
 
 function getYoutubeVideoId(url) {
-  if (!url) {
+  if (typeof url !== 'string') {
     return null;
   }
   var regExp = /^.*(youtu.be\/|v\/|e\/|u\/\w+\/|embed\/|v=)([^#\&\?]*).*/;
@@ -53,9 +53,6 @@ function getYoutubeVideoId(url) {
 }
 
 function getYoutubeThumbnailUrl(url) {
-  if (!url) {
-    return null;
-  }
   const videoId = getYoutubeVideoId(url);
   return videoId ? `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg` : null;
 }
